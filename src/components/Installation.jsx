8b18d4490388e05d218cc36999a454e939bf7d73
AkhilKompally <akhilkompally27@gmail.com>
2026-01-10 10:44:48 +0530

    feat:added installtion guide in a component section

--- src/components/Installation.jsx
+++ src/components/Installation.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { CopyLinear, TickCircleLinear, NoteTextLinear } from "./icons";
+import { CopyLinear, TickCircleLinear, NoteTextLinear } from "../icons";
 import "./Installation.css";
 
 function Installation() {
